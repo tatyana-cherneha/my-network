617e304b85e7a1d8b32bc69f0608774d82684f43
tcherneha <tatyana_cherneha@govitall.com>
2021-08-27 16:16:26 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+        <header className='header'>
+            <img src="" alt="logo"/>
+        </header>
+        <nav className='nav'>
+            <ul>
+                <li><a href="#p">Profile</a></li>
+                <li><a href="#d">Messages</a></li>
+                <li><a href="#n">News</a></li>
+                <li><a href="#m">Music</a></li>
+                <li><a href="#s">Setting</a></li>
+            </ul>
+        </nav>
+        <div className='content'>
+            <div className='bg-img'></div>
+            <div className='profile'>
+                ava + description
+            </div>
+            <div className='post'>
+                <div>My post</div>
+                <div>New post</div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
     </div>
   );
 }
