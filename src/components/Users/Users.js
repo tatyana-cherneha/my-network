d03f236502acb993591ed0bf1d56b4069136ab51
tcherneha <tatyana_cherneha@govitall.com>
2021-09-13 19:55:53 +0300

    l52 side effect

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,25 +4,19 @@ import * as axios from "axios";
 
 
 function Users(props) {
-     if (props.users.length === 0) {
+    let getUsers = () => {
+         if (props.users.length === 0) {
 
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-            props.setUsers(response.data.items);
-        });
-
-        // props.setUsers(
-        //     [
-        //         {id: 1, fullName: 'Kolya Ivanov', status: 'I am boss', location: {city: 'Kyev', country: 'Ukraine'}, photoUrl: 'https://upload.wikimedia.org/wikipedia/ru/1/16/Cruella_%282021%29_logo.jpg', followed: true},
-        //         {id: 2, fullName: 'Sacha Petrov', status: 'I am man', location: {city: 'Lviv', country: 'Ukraine'}, photoUrl: 'https://upload.wikimedia.org/wikipedia/ru/1/16/Cruella_%282021%29_logo.jpg', followed: true,},
-        //         {id: 3, fullName: 'Sveta Nikolaeva', status: 'I am woman', location: {city: 'Odessa', country: 'Ukraine'}, photoUrl: 'https://upload.wikimedia.org/wikipedia/ru/1/16/Cruella_%282021%29_logo.jpg', followed: false},
-        //         {id: 4, fullName: 'Alex Smith', status: 'I am hz', location: {city: 'Nikolaev', country: 'Ukraine'}, photoUrl: 'https://upload.wikimedia.org/wikipedia/ru/1/16/Cruella_%282021%29_logo.jpg', followed: false}
-        //     ]
-        // )
-     }
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                props.setUsers(response.data.items);
+            });
+         }
+    }
 
 
     return (<div className='user'>
+            <button onClick={getUsers}>Get Users</button>
             { props.users.map( u => <div id={u.id} key={u.id} className='user__info'>
                 <div className='user__info-item'>
                     <div className='user__info-avatar'>
