703febd8e561539230eb80ddcf14a6957f5fd3bd
tcherneha <tatyana_cherneha@govitall.com>
2021-09-13 20:24:40 +0300

    l54 componentDidMount

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -5,8 +5,8 @@ import * as axios from "axios";
 
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
