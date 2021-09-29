f1d11c7c96f17ebc877c440e27720fc30fef6a8e
tcherneha <tatyana_cherneha@govitall.com>
2021-09-29 20:20:07 +0300

    l74 redux form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,7 +1,45 @@
-const Login = (props) => {
+import './Login.scss'
+import { reduxForm, Field }from "redux-form";
+
+
+const LoginForm = (props) => {
+    const { handleSubmit } = props
     return (
-        <div>Login</div>
+        <div className='form'>
+            <form onSubmit={handleSubmit}>
+                <div className='form__control'>
+                    <Field placeholder='Input login' name="login" type="text" component="input" />
+                </div>
+                <div className='form__control'>
+                    <Field placeholder='Input password' name="password" type="password" component="input" />
+                </div>
+                <div className='d-flex'>
+                    <div className='form__checkbox'>
+                        <Field type="checkbox" component="input" name="rememberMe" />
+                        <span>Remember me</span>
+                    </div>
+                    <div className='form__button'>
+                        <button type="submit">Log in</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     )
 }
 
+let ReduxLoginForm = reduxForm({ form: 'login' })(LoginForm)
+
+const Login = () => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+
+            <ReduxLoginForm onSubmit={onSubmit} />
+
+        </div>
+    )
+}
 export default Login
--- src/redux/reduxStore.js
+++ src/redux/reduxStore.js
@@ -4,7 +4,8 @@ import dialogsReducer from "./dialogsReducer";
 import navbarReducer from "./navbarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -12,6 +13,7 @@ let reducers = combineReducers({
     navBar: navbarReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
